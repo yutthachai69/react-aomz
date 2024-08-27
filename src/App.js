359fa59eaf55f166bccdbf8c435aabdbb7e17619
Aomz <firstaza4455@gmail.com>
2024-08-27 16:31:27 +0700

    On branch master Changes to be committed: 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Button.js 	new file:   src/Mycss.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Buuton from "./Button";
+import {useState} from 'react';
 
 function App() {
+  const name = "Yutthachai Khammeephak";
+
+  const employees = [
+    { name: "AAAA", email: "AAAA@gmail.com", age: 20 },
+    { name: "BBBB", email: "BBBB@gmail.com", age: 21 },
+    { name: "CCCC", email: "CCCC@gmail.com", age: 22 },
+  ];
+  console.log(employees);
+  const[count,setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>{count}</h1>
+        <button onClick={()=> setCount(count +1)}>Count ++</button>
+        <button onClick={()=> setCount(count -1)}>Count --</button>
+        <h1>Hello {name}</h1>
+        {employees.map((em, index) => (
+          <li key={index}>
+            ชื่อพนักงาน: {em.name} | อีเมลล์: {em.email} | อายุ: {em.age}
+          </li>
+        ))}
+        <Buuton text="OK" />
+        <Buuton text="cancel" />
+        <Buuton text="calculas" />
+      </div>
+      <h1 className="text01">Test</h1>
+    </>
   );
 }
 
---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import './Mycss.css';
+function Buuton({text}) {
+    return ( 
+        <>
+         <button className='btn01'>{text}</button>
+        </>
+     );
+}
+
+export default Buuton;
